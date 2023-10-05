bf5fdfd2471b08a04d233cb96192d2514096fa49
Calebe Bastos <calebehonorato@outlook.com>
2023-10-05 09:26:43 -0300

    Também não sei
    sla

--- FibonacciTestão.js
+++ FibonacciTestão.js
@@ -1,14 +1,12 @@
-function Fibonacci (indice){
-var anterior = 0
-var atual = 1
+function Fibonacci(indice) {
+    var anterior = 0;
+    var atual = 1;
 
-for (var n = 0; n <= indice; n++){
-
-    proximo = (atual + anterior)
-    anterior = atual
-    atual = proximo
-    
+    for (var n = 0; n <= indice; n++) {
+        proximo = atual + anterior;
+        anterior = atual;
+        atual = proximo;
+    }
+    return proximo;
 }
-return proximo
-}
-console.log(Fibonacci(5))
+console.log(Fibonacci(5));
---
+++ teste.js
@@ -0,0 +1,5 @@
+var numero = 1;
+
+if (numero == 1) {
+    console.log('ih é 1 mesmo');
+}
